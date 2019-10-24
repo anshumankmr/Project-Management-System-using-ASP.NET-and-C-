1b1e90fe63073d94b3d279004c6c9536f3abc146
Anshuman Kumar <anshumankmr80@gmail.com>
2019-10-24 20:04:17 +0530

    Added Login
    Needs fixing

---
+++ Project Management/Project Management/Login.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Project_Management.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Project Management</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; WELCOME TO THE TASK MANAGEMENT SYSTEM<br />
+            <asp:Label ID="Label1" runat="server" Text="Enter Your Username"></asp:Label>
+&nbsp;
+            <br />
+&nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            Enter Your Password<br />
+&nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonLogin" runat="server" OnClick="Button1_Click" Text="Login" style="width: 60px" />
+
+
+                                                                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+                                                                      <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ Project Management/Project Management/Login.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Management
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private string connectionstring = WebConfigurationManager.ConnectionStrings["Project"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = TextBox1.Text;
+            string password = TextBox2.Text;
+            SqlConnection connection = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TABLE WHERE UserName = @username AND Password =  @password", connection);
+            cmd.Parameters.AddWithValue("@username",username);
+            cmd.Parameters.AddWithValue("@password", password);
+            SqlDataReader myreader;
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    myreader = cmd.ExecuteReader();
+                    myreader.Read();
+                    if (myreader != null )
+                    {
+                        Label2.Text = "Welcome " + myreader["name"] + "! " + "Redirecting You To " + myreader["Type"] + "'s Page";
+                        Thread.Sleep(2000);
+                       // Response.Redirect(myreader["Type"].ToString() + ".aspx");
+                    }
+                    else
+                    {
+                        Label2.Text = "Oops!! The Password You Entered Was Incorrect";
+                        Thread.Sleep(2000);
+                        //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Incorrect Password", "alert('Record Inserted Successfully')", true);
+                       // Page.Response.Redirect(Page.Request.Url.ToString(), true);
+                    }
+                }
+            }
+            catch(Exception)
+            {
+
+                Label2.Text = "Oops!! Random Error. Reloading in 1...";
+                //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Some Random Error. Reload?", "alert('Record Inserted Successfully')", true);
+               // Page.Response.Redirect(Page.Request.Url.ToString(), true);  
+            }
+        }
+    }
+}
